2af90ff1d25c72de54abfe46331860365061e67e
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-03-06 22:18:30 +0000

    80% done

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,24 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import imglink from "../../img/star_wars.webp";
+import imglink from "../../img/star_wars.png";
+import rigoImage from "../../img/rigo-baby.jpg"
 
 export const Navbar = () => {
 	return (
-		<div className=" bg-dark text-light">
-			<div className="row mt-4">
-				<div className="col">
-					<nav class="navbar navbar-light bg-dark justify-content-between">
-						<a class="navbar-brand"><i class="fa-brands fa-tiktok fa-xl fa-xl"></i></a><a><i class="fa-brands fa-instagram fa-xl"></i></a>
-						<a><i class="fa-brands fa-facebook"></i></a><a><i class="fa-brands fa-x-twitter fa-xl"></i></a> <a><i class="fa-brands fa-youtube fa-xl"></i></a>
-						<div className="col-8"></div>
-						<div className="col-1">
+		<div className=" text-light  pt-3 " style={{backgroundColor:"black"}}>
+			<div className="row ml-2 " style={{backgroundColor:"black"}}>
+				<div className="col ml-4">
+					<nav class="navbar navbar-light justify-content-between" style={{backgroundColor:"black"}}>
+						<a ><i class="fa-brands fa-tiktok fa-xl"></i></a>
+						<a><i class="fa-brands fa-instagram fa-xl"></i></a>
+						<a><i class="fa-brands fa-facebook"></i></a>
+						<a><i class="fa-brands fa-x-twitter fa-xl"></i></a> 
+						<a><i class="fa-brands fa-youtube fa-xl"></i></a>
+						<div className="col-7"></div>
+						<div className="col-2">
 
-							<input class="form-control mr-sm-10" type="search" placeholder="Search" aria-label="Search" />
+							<input class="form-control mr-m-6 w-100" type="search" placeholder="Search" aria-label="Search" />
 
 						</div>
 						<div className="col-1">
 
-							<button class="btn btn-outline-success mr-0" type="submit">Search</button>
+							<button class="btn btn-outline-secondary mr-0" type="submit">Search</button>
 
 						</div>
 					</nav>
@@ -31,6 +35,7 @@ export const Navbar = () => {
 				<div className="col-4"></div>
 				<div className="col">
 				<img className="star_img" src={imglink}/>
+			
 				</div>
 				<div className="col-4"></div>
 			
@@ -44,14 +49,15 @@ export const Navbar = () => {
 						<div class="container-fluid">
 							<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 								<div class="navbar-nav">
-									<a class="nav-link active" aria-current="page" href="#">News</a>
-									<a class="nav-link" href="#">Video</a>
-									<a class="nav-link" href="#">film</a>
-									<a class="nav-link" href="#">Series</a>
-									<a class="nav-link" href="#">Game</a>
-									<a class="nav-link" href="#">Disney</a>
+									<a class="nav-link active text-secondary" aria-current="page" href="#">News</a>
+									<a class="nav-link text-secondary" href="#">Video</a>
+									<a class="nav-link text-secondary" href="#">film</a>
+									<a class="nav-link text-secondary" href="#">Series</a>
+									<a class="nav-link text-secondary" href="#">Game</a>
+									<a class="nav-link text-secondary" href="#">Disney</a>
 
 								</div>
+								<hr/>
 							</div>
 						</div>
 					</nav>
--- src/js/views/demo.js
+++ src/js/views/demo.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
-import rigoImage from "../../img/rigo-baby.jpg";
+import imglink from "../../img/star_wars.png";
+import rigoImage from "../../img/rigo-baby.jpg"
 
 import { Context } from "../store/appContext";
 
