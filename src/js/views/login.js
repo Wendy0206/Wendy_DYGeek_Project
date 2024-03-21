3de34910be9e5f5874f7412f21defb240623f404
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-03-21 16:41:26 +0000

    done++

--- src/js/views/login.js
+++ src/js/views/login.js
@@ -7,6 +7,8 @@ import { AppContext } from "../layout";
 
 export const Login = () => {
     const [userN, setUserN] = useState('')
+    const [userLabel, setUserLabel] = useState(['Username', 'Email','Password', 'Log in'])
+    const [userU, setUserU] = useState('')
     const [userP, setUserP] = useState('')
     const [logInfo, setLogInfo] = useState([])
     const navigate = useNavigate();
@@ -17,7 +19,21 @@ export const Login = () => {
 
     }, []);
 
+
+    function createNewUser(){
+        let test= [' Create Username', 'Create Email',' Create Password', 'Create Account'];
+        setUserLabel(test)
+
+
+    }
+
     function get_username(val) {
+        let test = val.target.value;
+        setUserU(test)
+    }
+
+    
+    function get_email(val) {
         let test = val.target.value;
         setUserN(test)
     }
@@ -29,6 +45,7 @@ export const Login = () => {
 
 
     function login_function() {
+        if(userLabel[0]=='Username'){
 
         if (userN.length > 5 && userP.length >5) {
 
@@ -61,6 +78,46 @@ export const Login = () => {
             setUserP('')
         }
     }
+    else{
+        let test=['Username', 'Email','Password', 'Log in'];
+        setUserLabel(test)
+        if (userN.length > 5 && userP.length >5 && userU.length>5) {
+            fetch_newUser();
+        }
+        else {
+            alert('Please enter a valid username, email and/or password')
+            setUserN('')
+            setUserP('')
+            setUserU('')
+        }
+
+       
+
+
+    }
+function fetch_newUser(){
+   
+		let testArray = [userU,userN,userP];
+		fetch('https://turbo-rotary-phone-g44w56q9gw943pw7w-3000.app.github.dev/user/new', {
+			method: 'POST',
+			body: JSON.stringify(testArray),
+			headers: {
+				'Content-Type': 'application/json'
+			}
+		})
+			.then(res => {
+				if (!res.ok) throw Error(res.statusText);
+				return res.json();
+			})
+			.then(response => console.log('Success:', response))
+			.catch(error => console.error(error));
+
+
+	
+}
+
+
+    }
 
 
 
@@ -70,19 +127,26 @@ export const Login = () => {
        
                 <h3>Login Here</h3>
          
-                <label htmlFor="username">Username</label>
-                <input type="text" placeholder="Email or Phone" value={userN} id="username" onChange={(e) => get_username(e)} />
+                <form>
+                
+               <label htmlFor="username" style={userLabel[0]=='Username'? {display:'none' } : {display:'block' }}>{userLabel[0]}</label>
+               <input type="" placeholder="username" style={userLabel[0]=='Username'? {display:'none' } : {display:'block' }} value={userU} id="username" onChange={(e) => get_username(e)} />
 
-                <label for="password">Password</label>
+                <label htmlFor="email">{userLabel[1]}</label>
+                <input type="text" placeholder="Email or Phone" value={userN} id="email" onChange={(e) => get_email(e)} />
+
+                <label for="password">{userLabel[2]}</label>
                 <input type="password" placeholder="Password" value={userP} id="password" onChange={(e) => get_password(e)} />
+               
 
-                <button onClick={() => login_function()}>Log In</button>
+                <button onClick={() => login_function()}>{userLabel[3]}</button>
+              <span onClick={()=>createNewUser()} id='newaccount_text'>  <p  >Don't have an account? click here</p></span>
                 <div className="social">
                     <div className="go"><i className="fab fa-google"></i>  Google</div>
                     <div className="fb"><i className="fab fa-facebook"></i>  Facebook</div>
                 </div>
-            
-              
+          
+                </form>
         </div>
     );
 }
