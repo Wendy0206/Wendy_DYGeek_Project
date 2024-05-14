b4efeff6a125fa8ccbfbfc18bb204cb7a105cdff
Wendy0206 <155206385+Wendy0206@users.noreply.github.com>
2024-05-14 03:26:45 +0000

    Characters route added back

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -55,7 +55,7 @@ export const Navbar = () => {
 							<div className="navbar-nav">
 								<ul>
 									<li><span className="nav_link  nav_test" onClick={() => navigate('')}>News</span></li>
-									<li><span className="nav_link nav_test" onClick={() => navigate('/')}>Characters</span></li>
+									<li><span className="nav_link nav_test" onClick={() => navigate('/characters')}>Characters</span></li>
 									<li><span className="nav_link nav_test" onClick={() => navigate('/events')}>Events</span></li>
 									<li><span className="nav_link nav_test" onClick={() => navigate('/series')}>Series</span></li>
 								</ul>
